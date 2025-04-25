42e77b5e336fa6498b933091f4798d55ab9cfa7e
Tarcyo <55460663+Tarcyo@users.noreply.github.com>
2025-04-25 10:40:33 -0300

    Add files via upload

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.ifg_mobile_2"
+    namespace = "com.example.ifg_mobile"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = "27.0.12077973"
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.ifg_mobile_2"
+        applicationId = "com.example.ifg_mobile"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
